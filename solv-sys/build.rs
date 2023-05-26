329470db25f196de9af842c66a3618de042970e6
Benjamin Zeller <zeller.benjamin@web.de>
2023-05-26 09:10:54 +0200

    Build libsolv internally

--- solv-sys/build.rs
+++ solv-sys/build.rs
@@ -1,25 +1,97 @@
 extern crate bindgen;
 
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/home/zbenjamin/workspace/build-libsolv-Desktop_GCC-Debug/src/");
 
-    // Tell cargo to tell rustc to link the system solv library.
+    // cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE   -DCMAKE_BUILD_TYPE=Debug        -DWITH_LIBXML2=1        -DENABLE_APPDATA=1      -DENABLE_COMPS=1 -DENABLE_STATIC=1 -DDISABLE_SHARED=1 -DENABLE_SUSEREPO=1 -DENABLE_HELIXREPO=1 -DSUSE=1 -DENABLE_COMPLEX_DEPS=1 -DUSE_VENDORDIRS=1 -DENABLE_BZIP2_COMPRESSION=1 -DENABLE_ZSTD_COMPRESSION=1 -DENABLE_ZCHUNK_COMPRESSION=1
+
+    let libdir_path = PathBuf::from("libsolv")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    // our build directory
+    let libdir_build_path = PathBuf::from("build")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    // The path were we will build libsolv into
+    let lib_path = libdir_build_path.join("src");
+    let libext_path = libdir_build_path.join("ext");
+
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
+
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", libext_path.to_str().unwrap());
+
+    // Tell cargo to tell rustc to link the solv library.
     println!("cargo:rustc-link-lib=solv");
+
+    // Tell cargo to tell rustc to link the solvext library.
+    println!("cargo:rustc-link-lib=solvext");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
+
+    // create builddir
+    fs::create_dir_all(&libdir_build_path).expect("Failed to create builddir");
+
+    // generate makefiles with cmake
+    if !std::process::Command::new("cmake")
+        .current_dir( &libdir_build_path )
+        .arg("-DCMAKE_VERBOSE_MAKEFILE=TRUE")
+        .arg("-DCMAKE_BUILD_TYPE=Debug")
+        .arg("-DWITH_LIBXML2=1")
+        .arg("-DENABLE_APPDATA=1")
+        .arg("-DENABLE_COMPS=1")
+        .arg("-DENABLE_STATIC=1")
+        .arg("-DDISABLE_SHARED=1")
+        .arg("-DENABLE_SUSEREPO=1")
+        .arg("-DENABLE_HELIXREPO=1")
+        .arg("-DSUSE=1")
+        .arg("-DENABLE_COMPLEX_DEPS=1")
+        .arg("-DUSE_VENDORDIRS=1")
+        .arg("-DENABLE_BZIP2_COMPRESSION=1")
+        .arg("-DENABLE_ZSTD_COMPRESSION=1")
+        .arg("-DENABLE_ZCHUNK_COMPRESSION=1")
+        .arg(&libdir_path)
+        .output()
+        .expect("could not spawn `cmake`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not generate project files with cmake");
+    }
+
+
+    if !std::process::Command::new("make")
+        .current_dir( &libdir_build_path )
+        .arg("-j6")
+        .output()
+        .expect("could not spawn `make`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not compile libsolv");
+    }
+
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        .clang_arg("-F/home/zbenjamin/workspace/build-libsolv-Desktop_GCC-Debug/src")
-        .clang_arg("-F/home/zbenjamin/workspace/libsolv/src")
-        .clang_arg("-F/home/zbenjamin/workspace/libsolv/ext")
+        .clang_arg( ("-F".to_owned())+libdir_build_path.join("src").to_str().unwrap() )
+        .clang_arg("-F".to_owned()+libdir_path.join("src").to_str().unwrap() )
+        .clang_arg("-F".to_owned()+libdir_path.join("ext").to_str().unwrap())
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
